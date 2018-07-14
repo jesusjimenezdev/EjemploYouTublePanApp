e509d9bea65370ac1239ab9015fcf787cee00439
Gwinyai <gwinyai@gwinyaimacbook.local>
2018-07-14 19:44:56 +0200

    Initial Commit

---
+++ catchtrans/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  catchtrans
+//
+//  Created by Gwinyai on 14/7/2018.
+//  Copyright © 2018 Gwinyai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
